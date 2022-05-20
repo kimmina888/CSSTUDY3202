860a2808408d483eb2e4f6e1f29f2900a15d22b2
kim mina <kimmina9070@gmail.com>
2022-05-20 10:03:22 +0900

    #1 XML 주석 달기 ///를 사용하면 xml 주석을 달 수 있다. 주석 내용은 자동 완성에 표시 된다.

--- Car.cs
+++ Car.cs
@@ -4,21 +4,49 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace CSSTUDY3202
 {
+    /// <summary>
+    /// 자동차 주차장 입차, 출차 시간 등을 지정하는 클래스입니다.
+    /// </summary>
     class Car
     {
+        /// <summary>
+        /// 자동차 번호 입니다.
+        /// </summary>
         int carNumber;
+        /// <summary>
+        /// 자동차 입차 시간
+        /// </summary>
         DateTime inTime;
+        /// <summary>
+        /// 자동차 출차 시간
+        /// </summary>
         DateTime outTime;
+        
+        /// <summary>
+        /// 자동차 입차 시간을 지정합니다.
+        /// </summary>
         public void SetInTime()
         {
             this.inTime = DateTime.Now;
         }
-
+        /// <summary>
+        /// 자동차 출차 시간을 지정합닏.
+        /// </summary>
         public void SetOutTime()
         {
             this.outTime = DateTime.Now;
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="foo"></param>
+        /// <returns>foo * 10</returns>
+        public int some(int foo)
+        {
+            return foo * 10;
         }
     }
 }
