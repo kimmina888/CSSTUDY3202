df4d8b41d075c93f1084b8921c635b8f1d43fae5
kim mina <kimmina9070@gmail.com>
2022-05-26 11:47:20 +0900

    #클래스 리스트 연습 리스트에서 항목 제거할 때 인덱스 문제 해결하는 방법

--- Program.cs
+++ Program.cs
@@ -1,12 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using CSSTUDY3202;
 
-static void Main(string[] args) 
+static void Main(string[] args)
 {
     Car car = new Car();
     car.SetInTime();
     car.SetOutTime();
-    
+
     Random rand = new Random();
     Console.WriteLine(rand.Next(10, 100));
     Console.WriteLine(rand.Next(10, 100));
@@ -24,7 +24,7 @@ static void Main(string[] args)
       list.Add(32);
       list.Add(64);*/
     list.Remove(52);
-    foreach(var item in list)
+    foreach (var item in list)
     {
         Console.WriteLine("Count : " + list.Count + "/ titem " + item);
     }
@@ -42,9 +42,61 @@ static void Main(string[] args)
     Product productA = new Product();
     productA.name = "포켓몬 빵";
     productA.price = 1500;
-    Product productB = new Product() { name="소금빵", price=2000};
+    Product productB = new Product() { name = "소금빵", price = 2000 };
+    Product productC = new Product() { name = "보름달" };
 
-    Product productC = new Product() { name ="보름달"};
+    List<Student> students = new List<Student>()
+    {
+        new Student() { name = "함기훈", grade = 1 },
+        new Student() { name = "김다흰", grade = 2 },
+        new Student() { name = "김미나", grade = 2 },
+        new Student() { name = "김민정", grade = 3 },
+        new Student() { name = "김세린", grade = 3 },
+        new Student() { name = "김소연", grade = 1 },
+        new Student() { name = "김지수", grade = 1 },
+        new Student() { name = "김지호", grade = 2 },
+        new Student() { name = "김찬희", grade = 3 },
+        new Student() { name = "김하늘", grade = 2 },
+        new Student() { name = "김효리", grade = 1 }
+    };
+    /* students.Add(new Student() { name = "함기훈", grade = 1 });
+     students.Add(new Student() { name = "김다흰", grade = 2 });
+     students.Add(new Student() { name = "김미나", grade = 2 });
+     students.Add(new Student() { name = "김민정", grade = 3 });
+     students.Add(new Student() { name = "김세린", grade = 3 });
+     students.Add(new Student() { name = "김소연", grade = 1 });
+     students.Add(new Student() { name = "김지수", grade = 1 });
+     students.Add(new Student() { name = "김지호", grade = 2 });
+     students.Add(new Student() { name = "김찬희", grade = 3 });
+     students.Add(new Student() { name = "김하늘", grade = 2 });
+     students.Add(new Student() { name = "김효리", grade = 1 });*/
 
-
+    foreach (Student student in students)
+    {
+        Console.WriteLine(student);
+    }
+    /*foreach (Student student in students)
+    {
+        if (student.grade > 2)
+        {
+            students.Remove(student);
+        }
+    }
+    확인, 수정 ok
+    제거 x
+     */
+    for(int i =0; i < students.Count; i++)
+    {
+        if(students[i].grade > 2)
+        {
+            students.RemoveAt(i);
+        }
+    }
+    for (int i = students.Count - 1; i >= 0; i--)
+    {
+        if (students[i].grade > 2)
+        {
+            students.RemoveAt(i);
+        }
+    }
 }
---
+++ Student.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+internal class Student
+{
+    public string name;
+    public int grade;
+
+    public override string ToString()
+    {
+        return this.name + " : " + this.grade;
+    }
+    
+}
